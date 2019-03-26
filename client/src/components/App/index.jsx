3384a96654f45a367cd89f22dfa91dc310cc5b36
Unknown <jack.hoptz@gmail.com>
2019-03-26 16:20:56 +0000

    Added in the navigation and some basic styling

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
-import { withRouter, Switch, Route } from 'react-router-dom';
+// import React from 'react';
+// import { withRouter, Switch, Route } from 'react-router-dom';
 
-import { Home } from '../../components';
+// import { Navigation } from '../../components';
+
+// const App = (props) => {
+//   return (
+//     <Switch>
+//       <Route exact path="/" component={Navigation} />
+//     </Switch>
+//   )
+// }
+
+// export default withRouter(App);
+
+
+import React, { Component } from 'react';
+import { withRouter, Switch, Route, NavLink, HashRouter } from 'react-router-dom';
+import { Home, Stuff, Contact, Blog } from "../../components";
+import './App.scss';
+
 
 const App = (props) => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
-  )
-}
+        <div className="App">
+          <HashRouter>
+            <header>
+              <h1>Simple SPA</h1>
+              <ul className="header">
+                <li><NavLink exact to="/">Home</NavLink></li>
+                <li><NavLink to="/stuff">Stuff</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+                <li><NavLink to="/blog">Blog</NavLink></li>
+              </ul>
+              <div className="content">
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/stuff" component={Stuff}/>
+                <Route exact path="/contact" component={Contact}/>
+                <Route exact path="/blog" component={Blog}/>
+              </Switch>
+              </div>
+            </header>
+          </HashRouter>
+        </div>
+    );
+  }
 
 export default withRouter(App);
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,6 @@
 export { default as App } from './App';
-export { default as Home } from './Home';
 export { default as Article } from './Article';
+export { default as Blog } from './Blog';
+export { default as Contact } from './Contact';
+export { default as Home } from './Home';
+export { default as Stuff } from './Stuff';

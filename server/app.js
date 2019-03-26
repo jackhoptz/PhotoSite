9294479caf679a37492f103482c4f22a25999a2b
Unknown <jack.hoptz@gmail.com>
2019-03-26 14:15:21 +0000

    Backend and basic frontend added

---
+++ server/app.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'LightBlog', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+mongoose.connect('mongodb://localhost/lightblog');
+mongoose.set('debug', true);
+
+// Add models
+require('./models/Articles');
+// Add routes
+app.use(require('./routes'));
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+app.listen(8000, () => console.log('Server started on http://localhost:8000'));
---
+++ server/routes/api/articles.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+const router = require('express').Router();
+const Articles = mongoose.model('Articles');
+
+router.post('/', (req, res, next) => {
+  const { body } = req;
+
+  if(!body.title) {
+    return res.status(422).json({
+      errors: {
+        title: 'is required',
+      },
+    });
+  }
+
+  if(!body.author) {
+    return res.status(422).json({
+      errors: {
+        author: 'is required',
+      },
+    });
+  }
+
+  if(!body.body) {
+    return res.status(422).json({
+      errors: {
+        body: 'is required',
+      },
+    });
+  }
+
+  const finalArticle = new Articles(body);
+  return finalArticle.save()
+    .then(() => res.json({ article: finalArticle.toJSON() }))
+    .catch(next);
+});
+
+router.get('/', (req, res, next) => {
+  return Articles.find()
+    .sort({ createdAt: 'descending' })
+    .then((articles) => res.json({ articles: articles.map(article => article.toJSON()) }))
+    .catch(next);
+});
+
+router.param('id', (req, res, next, id) => {
+  return Articles.findById(id, (err, article) => {
+    if(err) {
+      return res.sendStatus(404);
+    } else if(article) {
+      req.article = article;
+      return next();
+    }
+  }).catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  return res.json({
+    article: req.article.toJSON(),
+  });
+});
+
+router.patch('/:id', (req, res, next) => {
+  const { body } = req;
+
+  if(typeof body.title !== 'undefined') {
+    req.article.title = body.title;
+  }
+
+  if(typeof body.author !== 'undefined') {
+    req.article.author = body.author;
+  }
+
+  if(typeof body.body !== 'undefined') {
+    req.article.body = body.body;
+  }
+
+  return req.article.save()
+    .then(() => res.json({ article: req.article.toJSON() }))
+    .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  return Articles.findByIdAndRemove(req.article._id)
+    .then(() => res.sendStatus(200))
+    .catch(next);
+});
+
+module.exports = router;
